ab63345c57e0eb512e86ce29ad136dd75a2fd8a0
BradlyGutierrez <bradly.gutierrez14520@est.uca.edu.ni>
2024-10-17 18:32:59 -0600

    Cambios en el app

--- src/app/Pages/login/page.js
+++ src/app/Pages/login/page.js
@@ -3,8 +3,9 @@ import AuthMenu from "@/app/components/login"
 const Login = () => {
     return (
         <div className="h-screen w-full bg-gray-100 flex flex-col justify-around items-center">
-            <p className=" font-bold text-3xl text-gray-500 flex flex-col w-1/2 text-center"> Hola bienvenido a tu app de recetas, inicia sesión para comenzar a crear tus recetas</p>
+            <p className=" font-bold text-3xl text-gray-500 flex flex-col w-1/2 text-center"> Hola bienvenido a tu app de recipy, inicia sesión para comenzar a crear tus recetas</p>
             <AuthMenu></AuthMenu>
+            {alert("Hola a todos")}
         </div>
     );
 
